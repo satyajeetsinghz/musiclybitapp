04fe5f9faf917873905d7bf501075a827d72f5b0
SATYA <work.satyajeetsingh@gmail.com>
2025-04-06 21:47:30 +0530

    Update: Playlist Ed Sheeran cover

--- src/assets/assets.tsx
+++ src/assets/assets.tsx
@@ -5,7 +5,7 @@ export const albums = [
     name: "Best of Sheeran",
     artist: "Ed Sheeran",
     image: "https://i.pinimg.com/originals/88/3b/2c/883b2c50f85db0b3af2ab8bbc610e4e4.jpg",
-    cover: "https://people.com/thmb/pLCPhXSSCKtpesgNZHfXN8MIISc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/Ed-sheeran-autumn-variations-artwork-cover-082423pg-9a117ec25b2a4778a2658855e5b59b28.jpg",
+    cover: "https://www.livemusic.sk/fotky/clanky/big/ed-sheeran-azizam-2025.jpg",
     aboutCover: "https://images-fe.ssl-images-amazon.com/images/S/pv-target-images/49b7de2367c880439acd0cdfd75b019f3d32e72e74324f2312a4cc3bdc3165bb._RI_TTW_SX1080_FMpng_.png",
 
     // Each album contains a list of songs with details like title, artist, and audio path
